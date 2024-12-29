a9b390615d80aa3349f9b15c46315e6a4eb63bd1
NoN Responsive WebDeveloper <gdbcodes@gmail.com>
2024-12-29 21:40:23 +0530

    search bar

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleMenu } from '../utils/appSlice';
 import { Link } from 'react-router-dom';
@@ -12,6 +12,41 @@ const Head = () => {
   const toggleHandler=()=>{
        dispatch(toggleMenu());
   }
+
+// Search suggestion api
+let search_api=import.meta.env.VITE_SEARCH
+
+
+const [searchQuery, setsearchQuery] = useState("")
+const [suggestion, setsuggestion] = useState([])
+const getsearchdata=async()=>{
+  
+   let blob=await fetch(`${search_api}${searchQuery}`)
+   let data=await blob.json();
+  //  setting the resonse suggestion in suggestion state
+   setsuggestion(data[1])
+   
+}
+
+
+
+useEffect(()=>{
+  // we will use debouncing here
+  // first make a api call for all searchs and 
+  // then restrict those searchs where the delay of 
+  // key press is <200 ms 
+  // getsearchdata();
+
+  // applying debouncing(<500ms)
+  let timerid=setTimeout(()=>getsearchdata(),300)
+ 
+  // in order to clear the timeout if another api request is made before the 
+  // assihned debounceing time 
+  return ()=>{
+    clearTimeout(timerid);
+  }
+
+},[searchQuery])
 
   return  (
     <div className='flex itemscenter justify-between px-2 py-5 shadow-sm' >
@@ -29,12 +64,20 @@ const Head = () => {
        
        
 
-      <div className='w-1/4'>
+      <div className='w-1/4 relative'>
         <input 
         className='border border-gray-300 py-[5px] outline-none w-3/4
-        rounded-l-full ' type="text" />
+        rounded-l-full p-3'
+        value={searchQuery}
+        onChange={(e)=>setsearchQuery(e.target.value)}
+         type="text" />
 
         <button className='py-[5px] px-4 border border-gray-300 rounded-r-full '>Search</button>
+        <div className='bg-gray-100 w-[90%] absolute  rounded-md shadow-lg' >
+          <ul>
+           {suggestion.map(data=> <li key={data} className='border border-b-2 p-2 hover:bg-gray-200 rounded-md' >🔍{data}</li>)}
+          </ul>
+        </div>
       </div>
 
       <div>
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,17 +2,21 @@ import React, { useEffect, useState } from 'react'
 import VideoCard from './VideoCard'
 import { Link } from 'react-router-dom'
 
-// YOutube video list api
-//  https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=[YOUR_API_KEY]
+
 
 const VideoContainer = () => {
 
   useEffect(()=>{
     getVideos();
+    
   },[])
-  const api=import.meta.env.VITE_API_KEY 
+
+  
+
+  const api=import.meta.env.VITE_API_KEY
+  const yt_api=import.meta.env.VITE_YT 
   const getVideos=async()=>{
-    let data=await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${api}`)
+    let data=await fetch(`${yt_api}${api}`)
     let videosdata=await data.json()
     setvideos(videosdata.items)
   }
